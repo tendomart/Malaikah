00d40046b3487c2751f1c8c11a270308d07fcc26
tendomart <tmkmart@gmail.com>
2019-07-04 08:30:29 +0300

    Added Project Files

---
+++ Malaikah/src/main/java/com/malaikahmedical/MalaikahApplication.java
@@ -0,0 +1,13 @@
+package com.malaikahmedical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MalaikahApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MalaikahApplication.class, args);
+	}
+
+}
