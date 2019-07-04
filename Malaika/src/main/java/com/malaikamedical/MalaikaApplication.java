a46d3e2589bbc6da8940615e7ee77ee61afc29f9
tendomart <tmkmart@gmail.com>
2019-07-04 09:03:33 +0300

    Made Changes to Folder Structure

---
+++ Malaika/src/main/java/com/malaikamedical/MalaikaApplication.java
@@ -0,0 +1,13 @@
+package com.malaikamedical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MalaikaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MalaikaApplication.class, args);
+	}
+
+}
